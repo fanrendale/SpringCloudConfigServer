bfbd5531043239c18ae18240242ba346c30c3d5d
fanrendale <1053314919@qq.com>
2019-04-03 18:41:14 +0800

    配置中心服务端

---
+++ src/main/java/com/xjf/configserver/SpringcloudConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.xjf.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心服务端
+ * @author Administrator
+ */
+//启用配置中心
+@EnableConfigServer
+@SpringBootApplication
+public class SpringcloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigServerApplication.class, args);
+    }
+
+}
